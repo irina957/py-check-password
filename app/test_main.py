2cb99e54622d810561e340a212010693aa8538f8
Iryna <irakirvas78@gmail.com>
2025-03-24 13:03:51 +0300

    Implement py-check-password

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize("passwords, expected",
+                         [("Pass@word1", True),
+                          ("pass!WOrd1", True),
+                          ("pass@WOrdd", False),
+                          ("Pass@", False),
+                          ("pass@word1", False),
+                          ("passsWOrd1", False),
+                          ("pass@WOrddpass@WOrddd", False),
+                          ("pass@WOrd1.", False),
+                          ("пар@WOrd1.", False)
+                          ])
+def test_check_passwords_with_different_data(
+        passwords: str, expected: bool) -> None:
+    assert check_password(passwords) == expected
