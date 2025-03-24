037eb53c23c993517b67b9d3f373e93e7a2817cf
Iryna <irakirvas78@gmail.com>
2025-03-24 13:22:20 +0300

    Implement py-check-password

--- app/test_main.py
+++ app/test_main.py
@@ -6,16 +6,17 @@ from app.main import check_password
                          [("Pass@word1", True),
                           ("pass!WOrd1", True),
                           ("pass@WOrdd", False),
-                          ("Pass@", False),
+                          ("Pass@1", False),
                           ("pass@word1", False),
                           ("passsWOrd1", False),
-                          ("pass@WOrddpass@WOrddd", False),
+                          ("pass@WOrddpass@WOrddd1", False),
                           ("pass@WOrd1.", False),
                           ("пар@WOrd1.", False),
                           ("Pass@w1r", True),
                           ("Pass@word1111111", True),
                           ("", False),
-                          ("Pard1$@#&!-_", True)
+                          ("Pard1$@#&!-_", True),
+                          ("Password1", False)
                           ])
 def test_check_passwords_with_different_data(
         passwords: str, expected: bool) -> None:
